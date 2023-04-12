384f65cb2c9be35f748504c0f720bd4d3461126a
Sergio <sergio@gmail.com>
2023-04-12 21:34:36 +0300

    add author_list

--- WebBooks/urls.py
+++ WebBooks/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     url(r'^books/$', views.BookListView.as_view(), name='books'),
     url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
-
+    url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
 ]
